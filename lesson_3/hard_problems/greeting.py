309ba8c905fc9289e4e06e90b613bf2a9dc7a5cc
Joshua Darovitz <joshdarovitz@gmail.com>
2025-05-31 21:26:18 -0400

    Added exercises for Lesson 3

--- lesson_3/hard_problems/greeting.py
+++ lesson_3/hard_problems/greeting.py
@@ -3,3 +3,14 @@ if False:
 
 print(greeting)
 #NameError, greeting variable not defined
+
+# In this code, we define a function with two paramaters string and value. The invocation implements a while loop,
+# but this while loop has no practical use as True is always truthy and break will always break the loop. 
+
+# string is called from the paramaters and will be assigned to a new object value. 
+
+# Since this is in the local scope and the invocation doesnt return any value, this function will simply not work as expected
+
+# global variable greet is assigned a string 'Hey', the function replace is called with arg greeting, "Hello). 
+# As discussed this function has no logical purpose
+# print(greet) will output the object greet is point to which in this case is the string 'Hey'
